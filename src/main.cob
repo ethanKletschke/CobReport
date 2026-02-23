4db3ffe64e39ae420347a3bf60c3c7c8d9f4ce89
Ethan Kletschke <bbruhingson@gmail.com>
2026-02-23 10:30:48 +0200

    feat: Define simple report layout

--- src/main.cob
+++ src/main.cob
@@ -25,6 +25,39 @@ FILE SECTION.
       05 FILLER PIC X(4) VALUE ALL SPACES.
       05 FL-Department PIC X(25).
 
+  *> Output report file.
+  FD Report-File
+    *> Link the report to its file.
+    REPORT IS Sales-Report.
+
+REPORT SECTION.
+  RD Sales-Report
+    PAGE LIMIT IS 36 LINES
+    HEADING 1
+    FIRST DETAIL 5
+    LAST DETAIL 36.
+    01 TYPE IS RH. *> Report Heading
+      05 LINE + 1.
+        10 COL 1 VALUE "========================================".
+      05 LINE + 1.
+        10 COL 1 VALUE "|                                      |".
+      05 LINE + 1.
+        10 COL 1 VALUE "|            Sales Report              |".
+      05 LINE + 1.
+        10 COL 1 VALUE "|            ~~~~~~~~~~~~              |".
+      05 LINE + 1.
+        10 COL 1 VALUE "========================================".
+
+    01 TYPE IS PH. *> Page Heading
+      05 LINE + 1.
+        10 COL 3 VALUE "".
+
+    01 TYPE PF. *> Page Footer
+      05 LINE + 1.
+        *> Page counter
+        10 COL 1 VALUE "Page".
+        10 COL + 2 SOURCE PAGE-COUNTER PIC Z9.
+
 PROCEDURE DIVISION.
   DISPLAY "CobReport v0.0.1".
   STOP RUN.
