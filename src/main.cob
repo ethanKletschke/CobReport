3b35f79dcb7e810f819491cd9939461749551bed
Ethan Kletschke <bbruhingson@gmail.com>
2026-02-23 10:31:58 +0200

    refactor(src): Add better indentation

--- src/main.cob
+++ src/main.cob
@@ -1,62 +1,62 @@
 >>SOURCE FORMAT FREE
 IDENTIFICATION DIVISION.
-PROGRAM-ID. CobReport.
+  PROGRAM-ID. CobReport.
 
 ENVIRONMENT DIVISION.
-INPUT-OUTPUT SECTION.
-FILE-CONTROL.
-  *> Input data file
-  SELECT Sales-File ASSIGN TO "Sales.dat"
-    ORGANISATION LINE SEQUENTIAL
-    ACCESS SEQUENTIAL.
+  INPUT-OUTPUT SECTION.
+    FILE-CONTROL.
+      *> Input data file
+      SELECT Sales-File ASSIGN TO "Sales.dat"
+        ORGANISATION LINE SEQUENTIAL
+        ACCESS SEQUENTIAL.
 
-  *> Output report file
-  SELECT Report-File ASSIGN TO "SalesReport.txt"
-    ORGANISATION SEQUENTIAL
-    ACCESS SEQUENTIAL.
+      *> Output report file
+      SELECT Report-File ASSIGN TO "SalesReport.txt"
+        ORGANISATION SEQUENTIAL
+        ACCESS SEQUENTIAL.
 
 DATA DIVISION.
-FILE SECTION.
-  FD Sales-File.
-    01 Sales-Record.
-      05 FL-Sale-Num PIC 9(5).
-      05 FILLER PIC X(4) VALUE ALL SPACES.
-      05 FL-Profit PIC $$,$$$,$$$.99.
-      05 FILLER PIC X(4) VALUE ALL SPACES.
-      05 FL-Department PIC X(25).
+  FILE SECTION.
+    FD Sales-File.
+      01 Sales-Record.
+        05 FL-Sale-Num PIC 9(5).
+        05 FILLER PIC X(4) VALUE ALL SPACES.
+        05 FL-Profit PIC $$,$$$,$$$.99.
+        05 FILLER PIC X(4) VALUE ALL SPACES.
+        05 FL-Department PIC X(25).
 
-  *> Output report file.
-  FD Report-File
-    *> Link the report to its file.
-    REPORT IS Sales-Report.
+    *> Output report file.
+    FD Report-File
+      *> Link the report to its file.
+      REPORT IS Sales-Report.
 
-REPORT SECTION.
-  RD Sales-Report
-    PAGE LIMIT IS 36 LINES
-    HEADING 1
-    FIRST DETAIL 5
-    LAST DETAIL 36.
-    01 TYPE IS RH. *> Report Heading
-      05 LINE + 1.
-        10 COL 1 VALUE "========================================".
-      05 LINE + 1.
-        10 COL 1 VALUE "|                                      |".
-      05 LINE + 1.
-        10 COL 1 VALUE "|            Sales Report              |".
-      05 LINE + 1.
-        10 COL 1 VALUE "|            ~~~~~~~~~~~~              |".
-      05 LINE + 1.
-        10 COL 1 VALUE "========================================".
+  REPORT SECTION.
+    RD Sales-Report
+      PAGE LIMIT IS 36 LINES
+      HEADING 1
+      FIRST DETAIL 5
+      LAST DETAIL 36.
+      01 TYPE IS RH. *> Report Heading
+        05 LINE + 1.
+          10 COL 1 VALUE "========================================".
+        05 LINE + 1.
+          10 COL 1 VALUE "|                                      |".
+        05 LINE + 1.
+          10 COL 1 VALUE "|            Sales Report              |".
+        05 LINE + 1.
+          10 COL 1 VALUE "|            ~~~~~~~~~~~~              |".
+        05 LINE + 1.
+          10 COL 1 VALUE "========================================".
 
-    01 TYPE IS PH. *> Page Heading
-      05 LINE + 1.
-        10 COL 3 VALUE "".
+      01 TYPE IS PH. *> Page Heading
+        05 LINE + 1.
+          10 COL 3 VALUE "".
 
-    01 TYPE PF. *> Page Footer
-      05 LINE + 1.
-        *> Page counter
-        10 COL 1 VALUE "Page".
-        10 COL + 2 SOURCE PAGE-COUNTER PIC Z9.
+      01 TYPE PF. *> Page Footer
+        05 LINE + 1.
+          *> Page counter
+          10 COL 1 VALUE "Page".
+          10 COL + 2 SOURCE PAGE-COUNTER PIC Z9.
 
 PROCEDURE DIVISION.
   DISPLAY "CobReport v0.0.1".
